de885ec1cf30660c978fd04e3f583054c8c8d3c3
Debashish Chakrborty <u4610248@anu.edu.au>
2019-04-17 17:30:13 +1000

    Running tests for lev novelties for real text

--- Test_Novelty.py
+++ Test_Novelty.py
@@ -21,8 +21,8 @@ def find_plot_novelties(test_sentences, corpus_sentences, novelty_file):
     print("Example test sentence: ", test_sentences[0])
 
     for sentence in tqdm(test_sentences, desc="Test sentences"):
-        novelties.append(novelty(sentence, corpus_sentences, 'jaccard'))
-        # novelties.append(novelty(sentence, corpus_sentences, 'levenshtein'))
+        # novelties.append(novelty(sentence, corpus_sentences, 'jaccard'))
+        novelties.append(novelty(sentence, corpus_sentences, 'levenshtein'))
 
     # Minimum novelty can be used to then find the sentence and potentially
     # discover reasons causing novelty to decrease
@@ -39,19 +39,21 @@ def find_plot_novelties(test_sentences, corpus_sentences, novelty_file):
         f.write(']')
     
 
-    # plot novelties against sentence
-    plt.plot(range(len(novelties)), novelties)
-    plt.xlabel('Sentence')
-    plt.ylabel('Novelty')
-    plt.show()
+    # # plot novelties against sentence
+    # plt.plot(range(len(novelties)), novelties)
+    # plt.xlabel('Sentence')
+    # plt.ylabel('Novelty')
+    # plt.show()
 
 if __name__ == '__main__':
     # save these sentences and novelties to save computation time
-    corpus_sentences = get_sentences(DATA_FILE)  # 304222 sentences
-    # print(len(corpus_sentences))
     test_sentences = get_sentences(TEST_FILE) # 10785 sentences
-    generated_sentences = get_sentences(GENERATED_FILE)
-    print(len(generated_sentences))
+    generated_sentences = get_sentences(GENERATED_FILE) # 11055 sentences
+
+    # save these sentences and novelties to save computation time
+    real_sentences = get_sentences(DATA_FILE)  # 304222 sentences
+
+    len_real = len(real_sentences)
 
     # print(test_sentences[9]) # They picked him off three times and kept him out of the end zone in a 22 - 6 victory at Arizona in 2013 .
 
@@ -60,8 +62,8 @@ if __name__ == '__main__':
     # print("Novelties for 3500 - 7000 sentences")
     # print("Novelties for 7000 - sentences")
 
-    find_plot_novelties(test_sentences, corpus_sentences, 
-                        novelty_file='novelties_real.txt')
+    find_plot_novelties(test_sentences, real_sentences[:len_real//10], 
+                        novelty_file='lev_novelties_real.txt')
 
     # find_plot_novelties(generated_sentences[:3500], corpus_sentences, 
     #                     novelty_file='LeakGAN_novelties_gen2_training_leven3500.txt')
