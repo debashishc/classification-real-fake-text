93ecc7b1ba1c1e5383d025624766564409f18881
debashishc <u4610248@anu.edu.au>
2019-05-08 07:53:07 +1000

    Running experiments for results and discussion in thesis

--- plotting.py
+++ plotting.py
@@ -77,3 +77,59 @@ def plot_confusion_matrix(cm,
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(
         accuracy, misclass))
     plt.show()
+
+    
+def plot_roc_curve(fprs, tprs):
+    """Plot the Receiver Operating Characteristic from a list
+    of true positive rates and false positive rates."""
+    
+    # Initialize useful lists + the plot axes.
+    tprs_interp = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    f, ax = plt.subplots(figsize=(14,10))
+    
+    # Plot ROC for each K-Fold + compute AUC scores.
+    for i, (fpr, tpr) in enumerate(zip(fprs, tprs)):
+        tprs_interp.append(np.interp(mean_fpr, fpr, tpr))
+        tprs_interp[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        ax.plot(fpr, tpr, lw=1, alpha=0.3,
+                 label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+        
+    # Plot the luck line.
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+             label='Luck', alpha=.8)
+    
+    # Plot the mean ROC.
+    mean_tpr = np.mean(tprs_interp, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+             label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+             lw=2, alpha=.8)
+    
+    # Plot the standard deviation around the mean ROC.
+    std_tpr = np.std(tprs_interp, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                     label=r'$\pm$ 1 std. dev.')
+    
+    # Fine tune and show the plot.
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Receiver operating characteristic')
+    ax.legend(loc="lower right")
+    plt.show()
+    return (f, ax)
+
+def compute_roc_auc(index):
+    y_predict = clf.predict_proba(X.iloc[index])[:,1]
+    fpr, tpr, thresholds = roc_curve(y.iloc[index], y_predict)
+    auc_score = auc(fpr, tpr)
+    return fpr, tpr, auc_score
