1dafa38038bb04fbe52d56faae806d2ef6167d11
debashishc <u4610248@anu.edu.au>
2019-05-19 22:25:37 +1000

    updated lstm classifier

--- classifier/LSTM/metrics_predictions.py
+++ classifier/LSTM/metrics_predictions.py
@@ -1,9 +1,3 @@
-__author__ = "Sandra Arcos Holzinger"
-__copyright__ = "Copyright 2019, Sentiment Analysis-BERT"
-__version__ = "1.0"
-__email__ = "Sandra.ArcosHolzinger@thalesgroup.com.au"
-__status__ = "Pre-Production"
-
 #-------------------------------------------------------------PACKAGES-------------------------------------------------------#
 from utils_LSTM import keras_predict
 import numpy as np
